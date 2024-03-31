cd2373dd81dcf019009c56bfc524e7fdf6917525
Renee Holliday <renee.holliday710@gmail.com>
2024-03-30 22:08:51 -0700

    modified:   CSPC323_P1.py

--- CSPC323_P1.py
+++ CSPC323_P1.py
@@ -1,60 +1,37 @@
+import re
 
-#TODO deal with comments, whitespace, actually try the shit
-
-keywords = {'if', 'return'}
-operators = {'>', '=', '+', '-', '*', '/', '<'}
-separators = {'(', ')', '{', '}', ';'}
-whitespace = {' '}  #not used currently but prob will need it later
-
-
-#yoinked this section for a prev assignment
 def lexical_analyzer(filename):
+    # Regular expressions to identify lexemes
+    keywords = {'if', 'return'}
+    operators = {'>', '=', '+', '-', '*', '/', '<'}
+    separators = {'(', ')', '{', '}', ';'}
+    whitespace = {' '}
+    
+    # Open the input file
     try:
         with open(filename, 'r') as file:
             input_text = file.read()
-            tokens = tokenize(input_text)
-            for token, token_type in tokens:
-                print(f'"{token}" = {token_type}')
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found.")
+        return
 
-if __name__ == "__main__":
-    filename = "CPSC323P1_input.txt"  #replace the file name for the file name
-    lexical_analyzer(filename)
-  
+    # Remove comments
+    input_text = re.sub(r'\/\/.*', '', input_text)
 
-def tokenize(input_text):
-    tokens = []          #holds the shit
-    i = 0  
-    
-    while i < len(input_text):  
-        
-        current_char = input_text[i]  
-        
-        if current_char in operators:
-            tokens.append((current_char, 'operator'))
-            i += 1
-            continue
-        
-        if current_char in separators:
-            tokens.append((current_char, 'separator'))
-            i += 1
-            continue
-        
-        if current_char.isdigit():
-            num = current_char
-            i += 1
-            tokens.append((num, 'integer'))
-            continue
-        
-        if current_char.isalpha(): 
-            identifier = current_char
-            i += 1
-            if identifier in keywords:
-                tokens.append((identifier, 'keyword'))
+    # Tokenize the input
+    for word in re.findall(r'[a-zA-Z0-9]+|[><=+\-*/();{}]', input_text):
+        if word in keywords:
+            print("'" + word + "'" + " = keyword")
+        elif word in operators:
+            print("'" + word + "'" + " = operator")
+        elif word in separators:
+            print("'" + word + "'" + " = seperator")
+        elif word not in whitespace:
+            if word.isalpha():
+                print("'" + word + "'" + " = identifier")
             else:
-                tokens.append((identifier, 'identifier'))
-            continue
-       
+                print("'" + word + "'" + " = integer")
     
-    return tokens
+
+# Call the lexical_analyzer function with the filename
+lexical_analyzer('input.txt')
